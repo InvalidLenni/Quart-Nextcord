70e918640c86a675d92c21d021230264b5024cd3
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-06-01 07:41:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import setup 
+requirements = [ 
+     'Quart', 
+     'pyjwt', 
+     'oauthlib', 
+     'Async-OAuthlib', 
+     'cachetools', 
+     'discord.py', 
+ ]
 
+on_rtd = os.getenv('READTHEDOCS') == 'True'
+if on_rtd: 
+     requirements.append('sphinxcontrib-napoleon') 
+     requirements.append('Pallets-Sphinx-Themes') 
+  
+ extra_requirements = { 
+     'docs': [ 
+         'sphinx==1.8.3' 
+     ] 
+ }
 
-setup()
+setup(extra_requirements=extra_requirements)
